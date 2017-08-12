e3199ac54f48723d66fa12576c1a4e6c9d7e912b
Junichi Takagi <junzzz@Junichi-no-MacBook-Pro.local>
2017-08-12 16:59:01 +0900

    wip

--- controllers/hell.go
+++ controllers/hell.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"database/sql"
 	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -15,32 +19,43 @@ type Person struct {
 }
 
 func HellWorld(w http.ResponseWriter, r *http.Request) {
-	// db, err := sql.Open("postgres", "postgres://junzzz:@localhost/postgres?sslmode=disable")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// rows, err := db.Query("SELECT id, name, address, rank FROM users;")
-	// persons := []Person{}
-	// for rows.Next() {
-	// 	p := Person{}
-	// 	err := rows.Scan(&p.ID, &p.Name, &p.Address, &p.Rank)
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	persons = append(persons, p)
-	// }
+	dbString := getDBString()
+	db, err := sql.Open("postgres", dbString)
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query("SELECT id, name, address, rank FROM users;")
+	persons := []Person{}
+	for rows.Next() {
+		p := Person{}
+		err := rows.Scan(&p.ID, &p.Name, &p.Address, &p.Rank)
+		if err != nil {
+			log.Fatal(err)
+		}
+		persons = append(persons, p)
+	}
 
-	// //t := template.New("sample templete")
-	// t, err := template.ParseFiles("/Users/junzzz/src/github.com/junzzz/hogee/templates/index.html")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// data := struct {
-	// 	Persons []Person
-	// }{
-	// 	Persons: persons,
-	// }
-	// err = t.Execute(w, data)
+	//t := template.New("sample templete")
+	t, err := template.ParseFiles("/Users/junzzz/src/github.com/junzzz/hogee/templates/index.html")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	data := struct {
+		Persons []Person
+	}{
+		Persons: persons,
+	}
+	err = t.Execute(w, data)
 	// //log.Println(err)
-	fmt.Fprint(w, "hell world")
+	//fmt.Fprint(w, "hell world")
+}
+
+func getDBString() string {
+	return fmt.Sprintf("postgres://%s:@%s:%s/%s?sslmode=%s",
+		os.Getenv("POSTGRES_USERNAME"),
+		os.Getenv("POSTGRES_HOSTNAME"),
+		os.Getenv("POSTGRES_PORT"),
+		os.Getenv("POSTGRES_DB_NAME"),
+		os.Getenv("POSTGRES_SSLMODE"),
+	)
 }
--- db/migrations/1_create_users.up.sql
+++ db/migrations/1_create_users.up.sql
@@ -1,6 +1,8 @@
 CREATE TABLE users (
-    id SERIAL
-    name text
-    address text
+    id SERIAL,
+    name text,
+    address text,
     rank integer
 );
+
+INSERT INTO users(name, address, rank) VALUES('docker太郎', '北海道', 99);
